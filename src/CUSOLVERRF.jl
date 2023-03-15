7e266773362d7b3ce3d29845a7e9dc3a918b8af5
François Pacaud <frapac@users.noreply.github.com>
2023-03-15 11:21:43 +0100

    bump to CUDA.jl 4.0 (#3)

--- src/CUSOLVERRF.jl
+++ src/CUSOLVERRF.jl
@@ -11,11 +11,6 @@ import CUDA.CUBLAS: unsafe_batch, unsafe_strided_batch
 
 import KLU
 
-# Headers
-# TODO: remove once the wrapper merged in CUDA.jl
-include("libcusolverRf_common.jl")
-include("libcusolverRf_api.jl")
-
 include("utils.jl")
 include("rf_wrapper.jl")
 include("klu.jl")
