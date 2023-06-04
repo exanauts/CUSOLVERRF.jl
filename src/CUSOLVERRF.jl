e3deeac36149d9ee496868a8a268473f7d51d94e
François Pacaud <frapac@users.noreply.github.com>
2023-06-04 20:58:21 +0200

    Add support for CUDA 12.1 (#9)

--- src/CUSOLVERRF.jl
+++ src/CUSOLVERRF.jl
@@ -11,11 +11,14 @@ import CUDA.CUBLAS: unsafe_batch, unsafe_strided_batch
 
 import KLU
 
+abstract type AbstractBacksolve end
+
 include("utils.jl")
 include("rf_wrapper.jl")
 include("klu.jl")
 
 include("backsolve.jl")
+include("backsolve_deprecated.jl")
 include("interface.jl")
 
 end # module
